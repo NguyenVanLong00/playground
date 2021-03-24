db38a5272c193d7904218f9c4fcee204e106a12b
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-03-24 18:08:08 +0700

    comit end day 24/3

--- doivo/main.js
+++ doivo/main.js
@@ -1,10 +1,6 @@
-$('.dropdown').mouseenter(function () {
-    if (!$('.navbar-toggle').is(':visible')) { // disable for mobile view
-        if (!$(this).hasClass('open')) { // Keeps it open when hover it again
-            $('.dropdown-toggle', this).trigger('click');
-        }
-    }
-});
+
+
+
 
 $('.news_container').hover(
     function () { $(this).children().slideDown(); },

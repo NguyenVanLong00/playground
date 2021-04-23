6db3eabca8c441b42335b5c3a2ff1d713fa9f949
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-04-23 14:29:24 +0700

    add categories

--- daily/main.js
+++ daily/main.js
@@ -1,7 +1,9 @@
 
 
 function flashSaleCountDown() {
-    // Set the date we're counting down to
+    if (document.getElementById("flash_sale_count_down") == null)
+        return;
+
     var countDownDate = Date.parse("2021-04-19T18:47:00.000");
 
     // Update the count down every 1 second
@@ -19,9 +21,9 @@ function flashSaleCountDown() {
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        days= days==0?"":days + ":";
-        
-        
+        days = days == 0 ? "" : days + ":";
+
+
         // Output the result in an element with id="demo"
         document.getElementById("flash_sale_count_down").innerHTML = days + hours + ":"
             + minutes + ":" + seconds;
@@ -29,10 +31,8 @@ function flashSaleCountDown() {
         // If the count down is over, write some text 
         if (distance <= 0) {
             clearInterval(x);
-            document.getElementById("flash_sale_count_down").innerHTML="Flash sale is over!"
+            document.getElementById("flash_sale_count_down").innerHTML = "Flash sale is over!"
         }
     }, 1000);
 }
-
-
 flashSaleCountDown();
